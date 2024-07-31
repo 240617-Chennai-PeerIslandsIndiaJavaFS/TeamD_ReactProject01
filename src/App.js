8dc3b80a8488d283d5945aea1d28e053b15212af
Pathivada-Varshini <varshinipathivada20@gmail.com>
2024-07-31 18:52:55 +0530

    display-messages completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import { UserContextComponent } from './components/Context/UserContextComponent';
 import Home from './components/HomePage/Home';
 import LoginPage from './components/loginpage/LoginPage';
+import DisplayMessages from './components/messages/DisplayMessages'
 
 function App() {
   return (
-      <Home/>
+      <DisplayMessages/>
   );
 }
 
