b753c84d1808343794661120457fa41112f3dd26
ELIGETI RAVI TEJA <eligetiraviteja267@gmail.com>
2024-08-18 00:17:43 +0530

    Integrated forget password functionality

--- src/components/forgotpasswordpage/ForgotPage.jsx
+++ src/components/forgotpasswordpage/ForgotPage.jsx
@@ -1,15 +1,71 @@
-// src/components/ForgotPasswordPage/ForgotPage.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './ForgotPage1.css';
-import ResetButton from '../buttons/ResetButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import image from '../../images/forget-password-image.png';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const ForgotPage = () => {
-    const handleResetClick = () => {
-        // Handle the reset logic here
-        console.log('Reset button clicked');
+    const [otp, saveOTP] = useState("");
+    const [email, setEmail] = useState("");
+    const [userOTP, setUserOTP] = useState("");
+    const [password, setPassword] = useState("");
+    const [cpassword, setCPassword] = useState("");
+    const navigate = useNavigate();
+    const [otpTrue, setOTP] = useState(true);
+
+    const sendOTP = (e) => {
+        e.preventDefault();
+        if (email === "") {
+            showError("Please enter email address", "success");
+        } else {
+            axios.get(`http://localhost:8080/api/employee/otp?email=${email}`)
+                .then((response) => {
+                    saveOTP(response.data.data);
+                    showError("Check your email for OTP", "success");
+                })
+                .catch((error) => {
+                    showError("Enter valid email", "success");
+                });
+        }
+    };
+
+    const verifyOTP = (e) => {
+        e.preventDefault();
+        if (otp === "") {
+            alert("Send OTP before verifying");
+        } else if (userOTP === "") {
+            showError("Enter the OTP before resetting password", "success");
+        } else if (otp === userOTP) {
+            setOTP(false);
+        } else {
+            alert(`Incorrect OTP: ${otp} : ${userOTP}`);
+        }
+    };
+
+    const resetPassword = (e) => {
+        e.preventDefault();
+        if (password === "") {
+            showError("Please enter a new password", "resetCheck");
+        } else if (password !== cpassword) {
+            showError("Password and confirm password do not match", "resetCheck");
+        } else {
+            axios.put(`http://localhost:8080/api/employee/reset?email=${email}&password=${password}`)
+                .then(() => {
+                    navigate("/login");
+                })
+                .catch(() => {
+                    showError("Error resetting password. Check your email or OTP.", "resetCheck");
+                });
+        }
+    };
+
+    const showError = (message, id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.style.display = "block";
+            element.innerHTML = message;
+        }
     };
 
     return (
@@ -20,21 +76,69 @@ const ForgotPage = () => {
                 </div>
                 <div className="col-12 col-md-6 col-lg-4 forgot-password-form">
                     <div className="forgot-password-form-container">
-                        <h2>Forgot Password</h2>
+                        <h2>Reset Password</h2>
                         <form>
-                            <div className="form-group">
-                                <input type="email" className="form-control" placeholder="Enter email" required />
-                            </div>
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Enter OTP" required />
-                            </div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" placeholder="New password" required />
-                            </div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" placeholder="Confirm password" required />
-                            </div>
-                            <Link className='btn btn-primary' to="/" value="Reset">Reset</Link>
+                            {
+                                otpTrue ?
+                                    <>  
+                                        <div className="form-group">
+                                            <input
+                                                type="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                name='email'
+                                                className="form-control"
+                                                placeholder="Enter email"
+                                                required
+                                            />
+                                        </div>
+                                        <div className='form-group'>
+                                            <button className='btn btn-primary' onClick={sendOTP}>Send OTP</button>
+                                        </div>
+                                        <div className="form-group">
+                                            <span id="success" style={{ color: "red", display: "none" }}>Check Email for OTP</span>
+                                            <input
+                                                type="text"
+                                                value={userOTP}
+                                                onChange={(e) => setUserOTP(e.target.value)}
+                                                name='otp'
+                                                className="form-control"
+                                                placeholder="Enter OTP"
+                                                required
+                                            />
+                                        </div>
+                                        <div className='form-group'>
+                                            <button className='btn btn-primary' onClick={verifyOTP}>Verify</button>
+                                        </div>
+                                    </>
+                                    :
+                                    <>
+                                        <div className="form-group">
+                                            <span id="resetCheck" style={{ color: "red", display: "none" }}></span>
+                                            <input
+                                                type="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                name='password'
+                                                className="form-control"
+                                                placeholder="New password"
+                                                required
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <input
+                                                type="password"
+                                                value={cpassword}
+                                                onChange={(e) => setCPassword(e.target.value)}
+                                                name='cpassword'
+                                                className="form-control"
+                                                placeholder="Confirm password"
+                                                required
+                                            />
+                                        </div>
+                                        <button className='btn btn-primary' onClick={resetPassword}>Reset</button>
+                                    </>
+                            }
                         </form>
                     </div>
                 </div>
@@ -42,4 +146,5 @@ const ForgotPage = () => {
         </div>
     );
 };
+
 export default ForgotPage;
